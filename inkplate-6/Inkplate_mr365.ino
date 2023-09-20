d1866e82f46e4dbf22762782ee380021ba2a3b39
James Futhey <kidGodzilla@users.noreply.github.com>
2023-09-20 10:15:45 -0400

    copy files

---
+++ inkplate-6/Inkplate_mr365.ino
@@ -0,0 +1,377 @@
+/*
+   Meeting Room 365 E-ink Display App
+   (c) 2022 James Futhey. All Rights Reserved.
+*/
+
+//////////////////////////////////
+//////// CONFIGURATION //////////
+////////////////////////////////
+
+// Wifi SSID & Password
+char ssid[] = "networkssid";
+char pass[] = "wifipassword";
+
+// Display Key
+char displayKey[] = "yourdisplaykeyhere";
+
+// Timezone String
+// If this is set, you don't need to provide an offset
+// ------------------------------------------------------------
+String timezone_string = ""; // None, or use your own custom Unix timezone string
+// String timezone_string = "America/New_York"; // Eastern, US
+// String timezone_string = "America/Chicago"; // Central, US
+// String timezone_string = "America/Denver"; // Mountain, US
+//String timezone_string = "America/Los_Angeles"; // Pacific, US
+// ------------------------------------------------------------
+
+// Timezone Offset, if timezone_string is not set
+int timeZone = -7;
+// ------------------------------------------------------------
+
+// Use a digital clock (vs. analog clock)
+int digitalclock = 2;
+
+// Refresh Interval (In Minutes)
+int update_clock_each_minute = 0; // Update clock more often than meetings are refreshed
+int minutes = 3; // Fetch events on this interval
+
+// Event refresh rate when on standby (seconds)
+#define TIME_TO_SLEEP 600
+
+// Touchscreen enabled start / end time
+int touch_start_hour = 0; // <= 0 touchscreen always enabled / disabled
+int touch_end_hour = 0; // >= 24 touchscreen always enabled, 0 always disabled
+
+// Show / hide battery indicator, battery %, temperature
+int temp_in_f = 1;
+int show_temperature = 0;
+int show_battery_icon = 1;
+int show_battery_percent = 1;
+int battery_icon_offset = 0; // Normally 0, 208 when right aligned
+
+// Battery voltage min/max
+const float battery_max = 4.60; // Maximum voltage of battery
+const float battery_min = 3.3;  // Minimum voltage before shutdown
+
+// Screen rotation, 1-4; 1 for Portrait, Default 4 for landscape 
+int rotation = 4; 
+
+
+//////////////////////////////////
+///// INTERNATIONALIZATION //////
+////////////////////////////////
+String extend = "Extend";
+String grab_room = "Grab Room";
+String end_meeting = "End Meeting";
+String please_wait_str = "Please Wait..";
+
+
+//////////////////////////////////
+// DO NOT EDIT BELOW THIS LINE //
+////////////////////////////////
+
+// Warn if not Inkplate 6PLUS
+#ifndef ARDUINO_INKPLATE6PLUS
+#error "Error: This program can only run on the Inkplate 6PLUS."
+#endif
+
+#include "Inkplate.h"
+#include "generatedUI.h"
+#include "driver/rtc_io.h"
+#include "Network.h"
+#include <algorithm>
+#include <ezTime.h>
+#include <ctime>
+
+RTC_DATA_ATTR int bootCount = 0;
+#define uS_TO_S_FACTOR 1000000
+unsigned long lastMillis;
+unsigned long lastMillis2;
+byte touchPadPin = PAD1;
+
+int battery_is_below_threshold = 0;
+float battery_threshold = 20.0;
+int use_digital_clock = 0;
+int wake_btn_pressed = 0;
+int show_reserve_btn = 0;
+int show_extend_btn = 0;
+int show_end_btn = 0;
+String voltstr = "";
+int please_wait = 0;
+int twentyfour = 0;
+int sleeping = 0;
+int occupied = 0;
+String data[11];
+int action = 0;
+int hours = 0;
+char date[64];
+
+Inkplate display(INKPLATE_3BIT);
+Network network;
+
+void setup() {
+    display.begin();
+    Serial.begin(115200);
+    display.frontlight(0);
+    display.setRotation(rotation);
+    
+    // Setup mcp interrupts
+    display.pinModeInternal(MCP23017_INT_ADDR, display.mcpRegsInt, touchPadPin, INPUT);
+    display.setIntOutput(1, false, false, HIGH);
+    display.setIntPin(touchPadPin, RISING);
+    if (!display.tsInit(true)) Serial.println("Touchscreen init failed!");
+    
+    action = 0;
+    ++bootCount;
+    displayInfo();
+    network.begin();
+    getDisplayData();
+}
+
+void readBattery() {
+    String charging = " ";
+    float battery_percent = 0.0;
+    int powered = display.readPowerGood();
+    float voltage = display.readBattery();
+    float temp = display.readTemperature();
+    voltstr = String(voltage, 1);
+    
+    // Serial debugging information
+    Serial.println("Hour of day:");
+    Serial.println(hours);
+    Serial.println("Voltage, powered, temp, batt<threshold:");
+    Serial.println(voltage);
+    Serial.println(powered);
+    Serial.print(display.readTemperature(), DEC);
+    Serial.println(" ");
+
+    // Convert voltage to percent
+    battery_percent = ((voltage - battery_min) / (battery_max - battery_min)) * 100;
+    if (battery_percent >= battery_threshold) battery_is_below_threshold = 0;
+    else battery_is_below_threshold = 1;
+    Serial.println(battery_is_below_threshold);
+    
+    rect02_b_x = 774 + (battery_percent / 4); // Dynamic icon "fill" width 774-799
+    if (battery_percent >= 100.0) battery_percent = 100.0;
+    if (battery_percent < 0.0) battery_percent = 0.0;
+    if (String(powered) == "255") charging = "+";
+    if (battery_percent == 100) charging = "";
+    text0_content = charging + String(battery_percent, 0) + "%";
+
+    if (show_temperature) {
+        show_battery_percent = 1;
+        if (temp_in_f) temp = temp * (9/5) + 32;
+        text0_content = " " + String(temp, 0) + " " + (temp_in_f ? "F" : "C");
+    }
+}
+
+void getTimeUpdateClock() {
+    // Timezone string to offset
+    if (timezone_string != "") {
+      Timezone LocalTime;
+      LocalTime.setLocation(String(timezone_string));
+      timeZone = LocalTime.getOffset() / 60 * -1;
+      Serial.println("timezone_string, timeZone");
+      Serial.println(timezone_string);
+      Serial.println(timeZone);
+    }
+    
+    network.getTime(date);
+
+    // Update clock
+    digital_clock0_h = widget1_h = ((date[0] - '0') * 10) + (date[1] - '0');
+    digital_clock0_m = widget1_m = ((date[3] - '0') * 10) + (date[4] - '0');
+    use_digital_clock = digitalclock;
+
+    // Update digital clock string (2)
+    if (digitalclock == 2) {
+      if (digital_clock0_h == 0) digital_clock0_h = 12;
+      text5c_content = String(digital_clock0_h) + ":" + (digital_clock0_m < 10 ? "0" : "") + String(digital_clock0_m);
+      text5c_cursor_x = digital_clock0_h >= 20 ? 790 : (digital_clock0_h >= 10 ? 800 : 823);
+    }
+}
+
+void updateOnlyClock() {
+    display.clearDisplay();
+    please_wait = 0;
+    readBattery();
+    sleeping = 0;
+  
+    getTimeUpdateClock();
+    hideButtonsIfSleeping();
+    
+    mainDraw();
+    display.display();
+    sleepOrDont();
+}
+
+void hideButtonsIfSleeping() {
+    // Hide buttons if we will deep sleep, exception on wake button press
+    if (wake_btn_pressed == 0 && (hours < touch_start_hour || hours >= touch_end_hour || battery_is_below_threshold == 1)) {
+      show_reserve_btn = 0;
+      show_extend_btn = 0;
+      show_end_btn = 0;
+      sleeping = 1;
+    }
+}
+
+void sleepOrDont() {
+    if (wake_btn_pressed == 1 || (hours > touch_start_hour && hours < touch_end_hour && battery_is_below_threshold == 0)) {
+      Serial.println("Touchscreen enabled");
+      // wake_btn_pressed = 0;
+      
+    } else {
+      Serial.println("Touchscreen disabled for 10 minutes");
+
+      // Deep sleep
+      display.tsShutdown();
+      rtc_gpio_isolate(GPIO_NUM_12);
+      esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+      esp_sleep_enable_ext0_wakeup(GPIO_NUM_36, 0);
+      esp_deep_sleep_start();
+    }
+}
+
+void getDisplayData() {
+    display.clearDisplay();
+    please_wait = 0;
+    readBattery();
+    sleeping = 0;
+    
+    while (!network.getData()) {
+        Serial.println("Retrying retriving data!");
+        delay(100);
+    }
+
+    getTimeUpdateClock();
+    hideButtonsIfSleeping();
+    
+    mainDraw();
+    display.display();
+    sleepOrDont();
+}
+
+void resetText() {
+    Serial.println("Resetting display");
+    display.clearDisplay();
+    display.display(1); // Immediate feedback, even if this executes slower
+    
+    // Reset all text to "loading" & empty state
+    use_digital_clock = digitalclock;
+    text1_content = "";
+    text2_content = "";
+    text5_content = please_wait_str;
+    text5b_content = "";
+    text6_content = "";
+    text7_content = "";
+    text8_content = "";
+    text9_content = "";
+    show_reserve_btn = 0;
+    show_extend_btn = 0;
+    show_end_btn = 0;
+    please_wait = 1;
+    
+    mainDraw();
+    display.display();
+}
+
+void keysEvents() {
+    // Portrait
+    if (rotation == 1 || rotation == 3) {
+      // Instant Reservation Press
+      if (display.touchInArea(500, 772, 245, 100) && show_reserve_btn == 1) {
+          Serial.println("Instant Reservation Pressed");
+          resetText();
+          action = 1; // Instant Reservation
+          getDisplayData();
+      }
+      // End Early Press
+      if (display.touchInArea(500, 900, 245, 100) && show_end_btn == 1) {
+          Serial.println("End Early Pressed");
+          resetText();
+          action = 2; // End early
+          getDisplayData();
+      }
+      // Extend Meeting Press
+      if (display.touchInArea(500, 782, 245, 100) && show_extend_btn == 1) {
+          Serial.println("Extend Meeting Pressed");
+          resetText();
+          action = 3; // Extend Meeting
+          getDisplayData();
+      }
+     
+    } else { // Landscape
+      // Instant Reservation Press
+      if (display.touchInArea(770, 185, 245, 100) && show_reserve_btn == 1) {
+          Serial.println("Instant Reservation Pressed");
+          resetText();
+          action = 1; // Instant Reservation
+          getDisplayData();
+      }
+      // End Early Press
+      if (display.touchInArea(770, 400, 245, 100) && show_end_btn == 1) {
+          Serial.println("End Early Pressed");
+          resetText();
+          action = 2; // End early
+          getDisplayData();
+      }
+      // Extend Meeting Press
+      if (display.touchInArea(770, 292, 245, 100) && show_extend_btn == 1) {
+          Serial.println("Extend Meeting Pressed");
+          resetText();
+          action = 3; // Extend Meeting
+          getDisplayData();
+      }
+    }
+
+    // Fetch new display data every n minutes
+    if (millis() - lastMillis >= minutes * 60 * 1000UL) {
+        Serial.println("Refreshing events due to timer");
+        lastMillis = millis();
+        wake_btn_pressed = 0;
+        action = 0;
+        getDisplayData();
+        
+    } else if (millis() - lastMillis2 >= 60 * 1000UL) {
+       // Handle 1 minute interval clock updates
+       lastMillis2 = millis();
+       if (update_clock_each_minute == 1) updateOnlyClock();
+    }
+}
+
+// Function that will write number of boots and boot reason to screen
+void displayInfo() {
+    Serial.print(F("Boot count: "));
+    Serial.println(bootCount, DEC);
+
+    // Display wake up reason
+    esp_sleep_wakeup_cause_t wakeup_reason;
+    wakeup_reason = esp_sleep_get_wakeup_cause();
+    
+    switch (wakeup_reason) {
+    case ESP_SLEEP_WAKEUP_EXT0:
+        Serial.println("Wakeup caused by external signal using RTC_IO");
+        wake_btn_pressed = 1;
+        resetText();
+        break;
+    case ESP_SLEEP_WAKEUP_EXT1:
+        Serial.println("Wakeup caused by external signal using RTC_CNTL");
+        break;
+    case ESP_SLEEP_WAKEUP_TIMER:
+        Serial.println("Wakeup caused by timer");
+        break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+        Serial.println("Wakeup caused by touchpad");
+        break;
+    case ESP_SLEEP_WAKEUP_ULP:
+        Serial.println("Wakeup caused by ULP program");
+        break;
+    default:
+        Serial.println("Wakeup was not caused by deep sleep");
+        break;
+    }
+}
+
+void loop() {
+    keysEvents();
+}
---
+++ inkplate-6/Network.cpp
@@ -0,0 +1,269 @@
+/*
+Network.cpp
+Inkplate 6 Arduino library
+David Zovko, Borna Biro, Denis Vajak, Zvonimir Haramustek @ e-radionica.com
+September 24, 2020
+https://github.com/e-radionicacom/Inkplate-6-Arduino-library
+
+For support, please reach over forums: forum.e-radionica.com/en
+For more info about the product, please check: www.inkplate.io
+
+This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
+Please review the LICENSE file included with this example.
+If you have any questions about licensing, please contact techsupport@e-radionica.com
+Distributed as-is; no warranty is given.
+*/
+
+#include "Network.h"
+
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+//#include "Inkplate.h"
+#include <ArduinoJson.h>
+
+extern char displayKey[];
+extern String text1_content;
+extern String text2_content;
+extern String text6_content;
+extern String text5_content;
+extern String text5b_content;
+extern String text7_content;
+extern String text8_content;
+extern String text9_content;
+extern String timezone_string;
+extern int show_reserve_btn;
+extern int show_extend_btn;
+extern int main_text_color;
+extern int show_end_btn;
+extern int rect0_color;
+extern String voltstr;
+extern int twentyfour;
+extern int occupied;
+extern int timeZone;
+extern int action;
+extern int hours;
+
+
+// Static Json from ArduinoJson library
+StaticJsonDocument<30000> doc;
+
+void Network::begin() {
+    // Initiating wifi
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+
+    int cnt = 0;
+    Serial.print(F("Waiting for WiFi to connect..."));
+    while ((WiFi.status() != WL_CONNECTED))
+    {
+        Serial.print(F("."));
+        delay(1000);
+        ++cnt;
+
+        WiFi.reconnect();
+        delay(5000);
+
+        if (cnt == 10)
+        {
+            Serial.println("Can't connect to WIFI, restarting");
+            delay(100);
+            ESP.restart();
+        }
+    }
+    Serial.println(F(" connected"));
+
+    // Find internet time
+    setTime();
+}
+
+void Network::getHours(char *timeStr) {
+    // Get seconds since 1.1.1970.
+    time_t nowSecs = time(nullptr);
+
+    // Used to store time
+    struct tm timeinfo;
+    gmtime_r(&nowSecs, &timeinfo);
+
+    // Copies time string into timeStr
+    strncpy(timeStr, asctime(&timeinfo) + 11, 5);
+
+    // Setting time string timezone
+    int hr = 10 * timeStr[0] + timeStr[1] + timeZone;
+
+    // Better defined modulo, in case timezone makes hours to go below 0
+    hr = (hr % 24 + 24) % 24;
+    
+    hours = hr;
+}
+
+void Network::getTime(char *timeStr) {
+    // Get seconds since 1.1.1970.
+    time_t nowSecs = time(nullptr);
+
+    // Used to store time
+    struct tm timeinfo;
+    gmtime_r(&nowSecs, &timeinfo);
+
+    // Copies time string into timeStr
+    strncpy(timeStr, asctime(&timeinfo) + 11, 5);
+
+    // Setting time string timezone
+    int hr = 10 * timeStr[0] + timeStr[1] + timeZone;
+
+    // Better defined modulo, in case timezone makes hours to go below 0
+    hr = (hr % 24 + 24) % 24;
+    
+    hours = hr;
+
+    if (twentyfour == 0 && hr > 12) {
+      hr -= 12;
+    }
+
+    // Adding time to '0' char makes it into whatever time char, for both digits
+    timeStr[0] = hr / 10 + '0';
+    timeStr[1] = hr % 10 + '0';
+}
+
+// Function to get display data from web
+bool Network::getData() {
+    // Variable to store fail
+    bool f = 0;
+
+    // If not connected to wifi reconnect wifi
+    if (WiFi.status() != WL_CONNECTED) {
+        WiFi.reconnect();
+
+        delay(5000);
+
+        int cnt = 0;
+        Serial.println(F("Waiting for WiFi to reconnect..."));
+        
+        while ((WiFi.status() != WL_CONNECTED)) {
+            // Prints a dot every second that wifi isn't connected
+            Serial.print(F("."));
+            delay(1000);
+            ++cnt;
+
+            WiFi.reconnect();
+            delay(5000);
+
+            if (cnt == 10) {
+                Serial.println("Can't connect to WIFI, restart initiated.");
+                delay(100);
+                ESP.restart();
+            }
+        }
+    }
+
+    // Http object used to make get request
+    HTTPClient http;
+
+    http.getStream().setTimeout(10);
+    http.getStream().flush();
+
+    // Begin http by passing url to it
+    // http.begin(calendarURL);
+
+    delay(300);
+
+    // Initiate http
+    char temp[512];
+    sprintf(temp, "https://api.mr365.co/eink/%s?action=%i&voltage=%s", displayKey, action, voltstr);
+
+    Serial.println(temp);
+
+    http.begin(temp);
+
+    // Actually do request
+    int httpCode = http.GET();
+    if (httpCode == 200) {
+        while (http.getStream().available() && http.getStream().peek() != '{')
+            (void)http.getStream().read();
+
+        // Try parsing JSON object
+        DeserializationError error = deserializeJson(doc, http.getStream());
+
+        if (error) {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.c_str());
+            f = 1;
+            
+        } else if (doc.size() > 0) {
+            // Set all data got from internet using formatTemp and formatWind defined above
+            // This part relies heavily on ArduinoJson library
+
+            Serial.println("Success");
+
+            // Save our data to data pointer from main file
+            text1_content = doc["name"].as<String>();
+            text2_content = doc["status"].as<String>();
+            text6_content = doc["currentMeetingTimeString"].as<String>();
+            text5_content = doc["currentMeetingSubjectLine1"].as<String>();
+            text5b_content = doc["currentMeetingSubjectLine2"].as<String>();
+            text7_content = doc["nextMeetingString"].as<String>();
+            text8_content = doc["nextMeetingSubject"].as<String>();
+            text9_content = doc["nextMeetingTimeString"].as<String>();
+            show_extend_btn = doc["canExtend"].as<int>();
+            show_end_btn = doc["canEndMeeting"].as<int>();
+            show_reserve_btn = doc["canInstantReserve"].as<int>();
+            occupied = doc["occupied"].as<int>();
+
+            if (doc["timezone"] && timezone_string == "") {
+              timeZone = doc["timezone"].as<int>();
+            }
+            if (doc["timezoneString"]) {
+              timezone_string = doc["timezoneString"].as<String>();
+            }
+            if (doc["twentyfour"]) {
+              twentyfour = doc["twentyfour"].as<int>();
+            }
+
+            Serial.println(occupied);
+            Serial.println(text1_content);
+            Serial.println(text2_content);
+            Serial.println(text6_content);
+            Serial.println(text5_content);
+            Serial.println(text5b_content);
+            Serial.println(text7_content);
+            Serial.println(text8_content);
+            Serial.println(text9_content);
+            Serial.println(show_extend_btn);
+            Serial.println(show_end_btn);
+            Serial.println(show_reserve_btn);
+            f = 0;
+        }
+    } else {
+       f = 1;
+    }
+
+    // Clear document and end http
+    doc.clear();
+    http.end();
+
+    return !f;
+}
+
+void Network::setTime() {
+    // Used for setting correct time
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    Serial.print(F("Waiting for NTP time sync: "));
+    time_t nowSecs = time(nullptr);
+    while (nowSecs < 8 * 3600 * 2) {
+        delay(500);
+        Serial.print(F("."));
+        yield();
+        nowSecs = time(nullptr);
+    }
+
+    Serial.println();
+
+    // Used to store time info
+    struct tm timeinfo;
+    gmtime_r(&nowSecs, &timeinfo);
+
+    Serial.print(F("Current time: "));
+    Serial.print(asctime(&timeinfo));
+}
